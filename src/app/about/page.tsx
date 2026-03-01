5d928f1e98ec899ab65a26cf083bfdc31ee581a3
benvanarragon <benjaminvanarragon@gmail.com>
2026-03-01 13:31:51 -0500

    init commit week 8

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <main>
+      <h1>About this Site</h1>
+      <p>We're building this for COMP2068 in Winter 2026 - Georgian.</p>
+    </main>
+  );
+}
---
+++ src/app/sushi/page.tsx
@@ -0,0 +1,7 @@
+export default function Sushi() {
+  return (
+    <main>
+      <h1>Our Sushi</h1>
+    </main>
+  );
+}
